0d3fc1c00e70ce05afe087178943c499491f018f
Ryan <ryangeorgebarker0@gmail.com>
2019-02-22 14:46:30 +0000

    added functionality to handle direction input

--- app/RoombaLogic.js
+++ app/RoombaLogic.js
@@ -3,11 +3,33 @@ var coordinateProcessor = require('./coordinateProcessor')
 module.exports = (roomDimensions, startCoords, directions, dirtCoords) => {
 
     let patchesCleaned = 0
-    let roombaLocation = startCoords
+    let roombaLocation = coordinateProcessor(startCoords)
 
     const directRoomba = (direction) => {
-        const location = coordinateProcessor(roombaLocation)
-        
+        const location = roombaLocation
+        // switch statement to change the location of the roomba based on the direction given
+        switch (direction) {
+            case 'N':
+                location['y']+=1
+                roombaLocation = location
+                break
+            case 'E':
+                location['x']+=1
+                roombaLocation = location
+                break
+            case 'S':
+                location['y']-=1
+                roombaLocation = location
+                break
+            case 'W':
+                location['x']-=1
+                roombaLocation = location
+                break
+            default:
+                console.log('default output')
+        }
     }
-    directRoomba('N')
+    directRoomba('E')
+
+    console.log(roombaLocation)
 }
--- app/coordinateProcessor.js
+++ app/coordinateProcessor.js
@@ -1,15 +1,14 @@
 module.exports = (coordinates) => {
 
-
         assignedCoordinates = {}
         // split the coordinates into individual values
         const elements = coordinates.split(' ')
         // assigns the coordinates to x or y based on their index
         elements.forEach((value, index) => {
             if (index === 0) {
-                assignedCoordinates['x'] = value
+                assignedCoordinates['x'] = parseInt(value)
             } else {
-                assignedCoordinates['y'] = value
+                assignedCoordinates['y'] = parseInt(value)
             }
         })
         return assignedCoordinates
